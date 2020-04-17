1bf2927dfe0535a3d01a5afbdef50cecbc3d7e32
wolfderby <OllieRadburn@gmail.com>
2020-04-16 23:52:41 -0400

    if the .iml file gives you problems delete it or do file > invalidate caches in android studio

--- build/app/generated/source/buildConfig/debug/com/example/flutteruilogin/BuildConfig.java
+++ build/app/generated/source/buildConfig/debug/com/example/flutteruilogin/BuildConfig.java
@@ -5,7 +5,7 @@ package com.example.flutteruilogin;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String APPLICATION_ID = "com.example.flutteruilogin";
+  public static final String APPLICATION_ID = "com.example.anahana_wellness";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
   public static final int VERSION_CODE = 1;
